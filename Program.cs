da9ee7edc6421d233a3c475cc01475121693d0c2
Denis Kiselyov <denis.kiselyov@gmail.com>
2024-07-06 19:45:43 +0700

    Initial revision

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using PhonePadTranslation;
+
+namespace Nokia3100
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PadTranslator padTranslator = new PadTranslator();
+
+            var result = padTranslator.OldPhonePad(args[0]);
+
+            Console.WriteLine(result);
+        }
+    }
+}
